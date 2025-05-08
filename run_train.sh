9eddb250f7c7b4353472b1c15abb4680df6ee08a
tkozakas <tomas.kozakas@gmail.com>
2025-05-08 12:31:45 +0300

    Remove source

--- run_train.sh
+++ run_train.sh
@@ -10,11 +10,9 @@
 
 # load UV Python & your venv
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
 
 uv python install 3.12
 uv venv --python 3.12
-source .venv/bin/activate
 
 uv pip install -r requirements.txt
 uv pip install torch torchvision torchaudio
