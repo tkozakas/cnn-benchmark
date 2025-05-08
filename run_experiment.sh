4469cde4fe113a46d227eb2e2258ee03ef607252
tkozakas <tomas.kozakas@gmail.com>
2025-05-08 12:32:39 +0300

    Remove source

--- run_experiment.sh
+++ run_experiment.sh
@@ -13,6 +13,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 uv python install 3.12
 uv venv --python 3.12
+source .venv/bin/activate
 
 uv pip install -r requirements.txt
 uv pip install torch torchvision torchaudio
